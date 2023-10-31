23f27231a867b2c51151beac8deb94cfb8426aed
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 21:00:31 +0400

    Added Unity IoC Helper class

---
+++ RouletteResults/IoCHelper.cs
@@ -0,0 +1,17 @@
+﻿using RouletteResults.Data.Services;
+using Unity;
+using Unity.Lifetime;
+
+namespace RouletteResults
+{
+    public static class IoCHelper
+    {
+        public static IUnityContainer Container { get; }
+        static IoCHelper()
+        {
+            Container = new UnityContainer();
+            Container.RegisterType<IPositionsRepository, PositionsRepository>(
+                new ContainerControlledLifetimeManager());
+        }
+    }
+}
