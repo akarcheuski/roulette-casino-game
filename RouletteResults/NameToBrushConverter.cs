4fcea3fc173174907ffae77034aa719a3ba91160
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 21:12:17 +0400

    Implemented NameToBrushConverter class

---
+++ RouletteResults/NameToBrushConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using RouletteResults.ViewModels;
+using Color = RouletteResults.Data.Color;
+
+namespace RouletteResults
+{
+    public class NameToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var input = GetColorNameByIdAsync((int?)value).Result.Name;
+            return input switch
+            {
+                "Red" => HexToBrush("#BC1318"),
+                "Black" => Brushes.Black,
+                "Blue" => HexToBrush("#00E6C9"),
+                "Green" => HexToBrush("#009B01"),
+                _ => DependencyProperty.UnsetValue
+            };
+        }
+
+        Brush HexToBrush(string hex) => (Brush) new BrushConverter().ConvertFrom(hex);
+
+        public async Task<Color> GetColorNameByIdAsync(int? id) => await PositionListVM._repo.GetColorByIdAsync(id);
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
