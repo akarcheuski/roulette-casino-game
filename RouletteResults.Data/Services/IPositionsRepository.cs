d3eed2466c4e258357becd3b39e96e22a826509a
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 20:45:56 +0400

    Added Repository pattern interface

---
+++ RouletteResults.Data/Services/IPositionsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RouletteResults.Data.Services
+{
+    public interface IPositionsRepository
+    {
+        Task<List<Position>> GetPositionsAsync();
+        Task<Color> GetColorByIdAsync(int? id);
+        Task<List<Dozen>> GetAllDozensAsync();
+        Task<List<Color>> GetAllColorsAsync();
+       // Task<Color> GetColorById(int id);
+    }
+}
