ffda187c6f1e99c32628ebdb30be4b317e5f5952
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 20:46:35 +0400

    Implemented the repository

---
+++ RouletteResults.Data/Services/PositionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RouletteResults.Data.Services
+{
+    public class PositionsRepository : IPositionsRepository
+    {
+        ResultsContext _ctx = new();
+        public Task<List<Position>> GetPositionsAsync() => _ctx.Positions.ToListAsync();
+        public Task<Color> GetColorByIdAsync(int? id) => _ctx.Colors.FirstOrDefaultAsync(c => c.Id == id);
+        public Task<List<Dozen>> GetAllDozensAsync()=> _ctx.Dozens.ToListAsync();
+        public Task<List<Color>> GetAllColorsAsync() => _ctx.Colors.ToListAsync();
+    }
+}
