f52803449c5f705bf40b66533e467b8adfe108a9
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 21:08:38 +0400

    Implemented MainWindowVM

---
+++ RouletteResults/ViewModels/MainWindowVM.cs
@@ -0,0 +1,21 @@
+﻿using RouletteResults.Data;
+using Unity;
+
+namespace RouletteResults.ViewModels
+{
+    internal class MainWindowVM : BaseModel
+    {
+        public MainWindowVM()
+        {
+            var positionListVm = IoCHelper.Container.Resolve<PositionListVM>();
+            MainVM = positionListVm;
+        }
+
+        private BaseModel _MainVM;
+        public BaseModel MainVM
+        {
+            get => _MainVM;
+            set => SetProperty(ref _MainVM, value);
+        }
+    }
+}
