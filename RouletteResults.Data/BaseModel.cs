2de0e677d01dcaefd221b82769bf2fd758e8bbb5
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 20:38:03 +0400

    Created Base Model class

---
+++ RouletteResults.Data/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RouletteResults.Data
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged(string prop) => PropertyChanged(this, new PropertyChangedEventArgs(prop));
+
+        protected virtual void SetProperty<T>(ref T member, T val,
+            [CallerMemberName] string name = null)
+        {
+            if (Equals(member, val)) return;
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected static string ThrowNullEx(string msg) => throw new ArgumentNullException(msg);
+    }
+}
