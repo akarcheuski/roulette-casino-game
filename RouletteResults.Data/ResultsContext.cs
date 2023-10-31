6469d5fbf46be15d5ae2667df3fa310c6d87d4af
Andrei Karcheuski <andrei@tileg.com>
2023-10-31 20:44:13 +0400

    Added DB context class

---
+++ RouletteResults.Data/ResultsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace RouletteResults.Data
+{
+    public class ResultsContext : DbContext
+    {
+        const string DbPath = @"RouletteResults.Data\Sql\RouletteResultsData.db";
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Dozen> Dozens { get; set; }
+ 
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            var projPath = Path.GetDirectoryName(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName);
+            ob.UseSqlite($"Data Source={Path.Combine(projPath, DbPath)}");
+            // ob.UseSqlite("FileName=RouletteResultsData.db");
+        }
+    }
+}
