ac186020b1d12f90faa1d6954958c491c8259331
Andrei Karcheuski <andrei@tileg.com>
2023-11-01 19:23:58 +0400

    Updated the connectionString

--- RouletteCasinoGame/RouletteResults.Data/ResultsContext.cs
+++ RouletteCasinoGame/RouletteResults.Data/ResultsContext.cs
@@ -7,16 +7,12 @@ namespace RouletteResults.Data
 {
     public class ResultsContext : DbContext
     {
-        const string DbPath = @"RouletteResults.Data\Sql\RouletteResultsData.db";
         public DbSet<Position> Positions { get; set; }
         public DbSet<Color> Colors { get; set; }
         public DbSet<Dozen> Dozens { get; set; }
  
-        protected override void OnConfiguring(DbContextOptionsBuilder ob)
-        {
-            var projPath = Path.GetDirectoryName(Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName);
-            ob.UseSqlite($"Data Source={Path.Combine(projPath, DbPath)}");
-            // ob.UseSqlite("FileName=RouletteResultsData.db");
-        }
+        protected override void OnConfiguring(DbContextOptionsBuilder ob) =>
+            ob.UseSqlite("FileName=Sql\\RouletteResultsData.db");
+       
     }
 }
